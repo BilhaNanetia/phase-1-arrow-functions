313353cbf1c24d3e8b234da91e067e8615a4f56c
Bilha <bilha.leposo@student.moringaschool.com>
2024-03-23 16:59:15 +0300

    complete arrow functions

---
+++ index.js
@@ -0,0 +1,26 @@
+//Arrow function for division
+const divide =(a, b) => a / b ;
+
+//using divide
+let a = 2000;
+let b = 100;
+let result = divide(a, b)
+console.log(result); // output: 20;
+
+//Arrow function for squaring
+const square = x => x * x;
+
+//Using squaring
+let x = 2;
+let squaredValue = square (x);
+console.log(squaredValue) ;  // output: 4;
+
+//Arrow function for addition
+const add = (a, b) => a + b;
+
+//Example using addition
+let A = 3;
+let B = 4;
+let sum=add(3, 4);
+console.log(sum);  // output: 7;
+
